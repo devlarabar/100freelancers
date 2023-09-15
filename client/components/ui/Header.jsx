2f879c18d318c7746a98cf84c32e5b27d5a619cb
Karissa Lange <112651523+ka-lange@users.noreply.github.com>
2023-09-15 15:53:49 -0400

    re-color menu dropdown to have more contrast; added another link to route to homepage

--- client/components/ui/Header.jsx
+++ client/components/ui/Header.jsx
@@ -29,7 +29,7 @@ const Header = ({ ThemeToggle }) => {
             <Link href={homeLink} className="btn btn-ghost normal-case text-xl"><span className="text-accent">100</span>freelancers</Link>
             <div className="">
                 {auth?.user ? (
-                    <div className="dropdown dropdown-end">
+                    <div className="dropdown dropdown-end text-secondary">
                         <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
                                 <Image
@@ -41,7 +41,8 @@ const Header = ({ ThemeToggle }) => {
                                 />
                             </div>
                         </label>
-                        <ul tabIndex="0" className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <ul tabIndex="0" className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-primary border border-secondary rounded-box w-52">
+                            <li><Link href={homeLink}>Home</Link></li>
                             <li>
                                 <Link href={'/profile'} className="justify-between">
                                     Profile
