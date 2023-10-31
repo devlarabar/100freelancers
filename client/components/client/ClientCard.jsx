dc9f39b166a412935a5b380d1c587685de91ed5c
Ramadan E. Raji <armadanraji@gmail.com>
2023-10-31 10:20:02 -0500

    revised the client dropdown menu using headlessUI

--- client/components/client/ClientCard.jsx
+++ client/components/client/ClientCard.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Menu } from "@headlessui/react";
+import DropdownCard from "./DropdownCard";
+
 import {
   EllipsisHorizontalIcon,
   PhoneIcon,
@@ -6,36 +9,17 @@ import {
   MapPinIcon,
 } from "@heroicons/react/24/outline";
 const ClientCard = ({ client }) => {
-  const [isOpen, setisOpen] = useState(false);
   return (
     <section className="client-card">
       <div>
         <div className="relative flex justify-between">
           <h3 className="">{client.name}</h3>
-          <EllipsisHorizontalIcon
-            className="h-8 w-8 cursor-pointer stroke-1 text-accent hover:stroke-2"
-            onClick={() => setisOpen(!isOpen)}
-          />
-          {isOpen && (
-            <div className="w-40 z-[1] border-secondary rounded-lg absolute left-full mt-2 origin-top-right  rounded-md bg-primary   shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none shadow">
-              <a className="group flex items-center px-4 py-2 text-sm text-CCCFD2-700 hover:bg-[rgb(205,213,224,0.1)] hover:text-white hover:rounded-lg">
-                {" "}
-                <span>add outreach</span>
-              </a>
-              <a className="group flex items-center px-4 py-2 text-sm text-CCCFD2-700 hover:bg-[rgb(205,213,224,0.1)] hover:text-white hover:rounded-lg">
-                {" "}
-                <span>edit</span>
-              </a>
-              <a className="group flex items-center px-4 py-2 text-sm text-CCCFD2-700 hover:bg-[rgb(205,213,224,0.1)] hover:text-white hover:rounded-lg">
-                {" "}
-                <span>archive</span>
-              </a>
-              <a className="group flex items-center px-4 py-2 text-sm text-CCCFD2-700 hover:bg-[rgb(205,213,224,0.1)] hover:text-white hover:rounded-lg">
-                {" "}
-                <span>delete</span>
-              </a>
-            </div>
-          )}
+          <Menu>
+            <Menu.Button>
+              <EllipsisHorizontalIcon className="h-8 w-8 cursor-pointer stroke-1 text-accent hover:stroke-2" />
+            </Menu.Button>
+            <DropdownCard />
+          </Menu>
         </div>
 
         <hr />
