0ea39d18bd1a4ea274bb8e0432286dbaf7dbd5fe
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2023-11-08 20:39:33 -0500

    added screenshot image files to public folder and added their code to page.jsx.  Tailwind code is still divided between the global.css and page.jsx.  Next steps are to continue the styling on the screenshots to match the example layout.

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -8,10 +8,7 @@ export default function Home() {
     if (auth?.user) return redirect('/home')
     return (
         <main className = "landing-page">
-            <h2>Welcome to 100freelancers!</h2>
-            <p>Organize and keep track of freelance client outreach.</p>  {/*Added quick discription of what 100Freelancers is*/}
-
-            {/* Adding the elements from the homepage layout.  Test-Screenshot.png is located in the 'public' folder */}
+            {/* Adding the elements from the homepage layout.  images located in the 'public' folder */}
             <section className="floats">
                 <ul>
                     <li>
@@ -27,12 +24,18 @@ export default function Home() {
                         <p>ONGOING OUTREACH</p>
                     </li>
                 </ul>
-                <img
-                    src="/Test-Screenshot.png"
-                    alt='Screenshot of 100freelancers pages'
-                    // width={1000}
-                    // height={1000}
-                />
+                <div className="screenshots flex w-2/3 justify-center items-end ">
+                    <img
+                        className="outreach h-96 "
+                        src="/outreach-one.png"
+                        alt='Screenshot of 100freelancers outreach page'
+                    />
+                    <img 
+                        className="full-screen w-11/12"
+                        src="/full-screen.png" 
+                        alt="Screenshot of 100freelancers client tiles" 
+                    />
+                </div>
             </section>
         </main>    
     )
