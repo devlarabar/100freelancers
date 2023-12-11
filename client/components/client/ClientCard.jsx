3fa53ca4d3f2668c43fef1cb34c284faa4a82fb2
Lara Alexander <lara.alexander@hotmail.com>
2023-12-11 16:55:07 -0500

    Adjusted spacing on ClientCard.jsx

--- client/components/client/ClientCard.jsx
+++ client/components/client/ClientCard.jsx
@@ -10,35 +10,35 @@ import {
 } from "@heroicons/react/24/outline"
 
 const ClientCard = ({ client, view }) => {
-	if(view === 'list-view'){
-			return (
-					<section className="bg-primary rounded w-full p-4 my-3">
-							<div className="flex justify-between items-center">
-									<div className="w-3/12 pr-2 border-r border-accent">
-											<h5 className="font-bold text-sm sm:text-xl">{client.businessName}</h5>
-											<span className="text-sm italic">{client.businessType}</span>
-									</div>
-									<div className="flex flex-col w-9/12 ps-4 md:flex-row md:items-center md:justify-center">
-											<div className="contact-info-group md:w-4/12 md:mb-0">
-													<PhoneIcon className="contact-icon-list me-2" />
-													<p className="text-xs sm:text-base">{client.phone}</p>
-											</div>
-											<div className="contact-info-group md:w-4/12 md:mb-0">
-													<EnvelopeIcon className="contact-icon-list me-2" />
-													<p className="text-xs sm:text-base break-all">{client.email}</p>
-											</div>
-											<div className="contact-info-group md:w-4/12 md:mb-0">
-													<MapPinIcon className="contact-icon-list me-2" />
-													<p className="text-xs sm:text-base">{client.address}</p>
-											</div>
-									</div>
-									<ClientCardDropdown />
-							</div>
+	if (view === 'list-view') {
+		return (
+			<section className="bg-primary rounded w-full p-4 my-3">
+				<div className="flex justify-between items-center">
+					<div className="w-3/12 pr-2 border-r border-accent">
+						<h5 className="font-bold text-sm sm:text-xl">{client.businessName}</h5>
+						<span className="text-sm italic">{client.businessType}</span>
+					</div>
+					<div className="flex flex-col w-9/12 ps-4 md:flex-row md:items-center md:justify-center">
+						<div className="contact-info-group md:w-4/12 md:mb-0">
+							<PhoneIcon className="contact-icon-list me-2" />
+							<p className="text-xs sm:text-base">{client.phone}</p>
+						</div>
+						<div className="contact-info-group md:w-4/12 md:mb-0">
+							<EnvelopeIcon className="contact-icon-list me-2" />
+							<p className="text-xs sm:text-base break-all">{client.email}</p>
+						</div>
+						<div className="contact-info-group md:w-4/12 md:mb-0">
+							<MapPinIcon className="contact-icon-list me-2" />
+							<p className="text-xs sm:text-base">{client.address}</p>
+						</div>
+					</div>
+					<ClientCardDropdown />
+				</div>
 			</section>
 		);
 	}
 
-else return (
+	else return (
 		<section className="client-card">
 			<div>
 				<div className="relative flex justify-between">
